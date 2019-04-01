67753d39fae1550d452f174138c6db8ab010eec2
장경재 <secrecy418@naver.com>
2019-04-01 13:28:06 +0900

    메인 페이지 redux 기능 추가

--- src/components/shop/ShopList.js
+++ src/components/shop/ShopList.js
@@ -10,64 +10,38 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome'
 import {Actions} from 'react-native-router-flux';
 import PreventDoubleClick from '../common/PreventDoubleClick';
+import {connect} from 'react-redux';
+import * as shopActions from '../../store/modules/shop/shop';
+import {bindActionCreators} from "redux";
 
-const TouchableWithoutFeedback2= PreventDoubleClick(TouchableWithoutFeedback);
+const TouchableWithoutFeedback2 = PreventDoubleClick(TouchableWithoutFeedback);
 
 class ShopList extends Component {
-    state = {
-        shopNames: [
-            {
-                'id': '1',
-                'name': '레귤러식스',
-                'introduction': '레귤러식스는 강남 N타워 B2에 위치한 복합공간으로, 외식문화를 선도하는 공간입니다',
-                'uri': 'https://cdn.pixabay.com/photo/2015/06/30/18/36/st-826688_1280.jpg'
-            },
-            {
-                'id': '2',
-                'name': '평화옥',
-                'introduction': '평화옥은 강남 N타워 B2에 위치한 복합공간으로, 외식문화를 선도하는 공간입니다',
-                'uri': 'https://cdn.pixabay.com/photo/2014/04/26/00/41/dining-room-332207_1280.jpg'
-            },
-            {
-                'id': '3',
-                'name': '조선횟집',
-                'introduction': '평화옥은 강남 N타워 B2에 위치한 복합공간으로, 외식문화를 선도하는 공간입니다',
-                'uri': 'https://cdn.pixabay.com/photo/2015/06/30/18/35/st-826687_1280.jpg'
-            },
-            {
-                'id': '4',
-                'name': '라운지엑스',
-                'introduction': '평화옥은 강남 N타워 B2에 위치한 복합공간으로, 외식문화를 선도하는 공간입니다',
-                'uri': 'https://cdn.pixabay.com/photo/2016/08/28/22/02/restaurant-1626983_1280.jpg'
-            },
-            {
-                'id': '5',
-                'name': '라운지',
-                'introduction': '평화옥은 강남 N타워 B2에 위치한 복합공간으로, 외식문화를 선도하는 공간입니다',
-                'uri': 'https://cdn.pixabay.com/photo/2014/06/23/19/56/breakfast-room-375489_1280.jpg'
-            },
-            {
-                'id': '6',
-                'name': '평화옥',
-                'introduction': '평화옥은 강남 N타워 B2에 위치한 복합공간으로, 외식문화를 선도하는 공간입니다',
-                'uri': 'https://cdn.pixabay.com/photo/2015/06/30/18/36/st-826688_1280.jpg'
-            },
-        ]
-    };
+
+    handleSelectShop = (shopName,index) => {
+        const {ShopActions} = this.props;
+        console.log("index : ", index)
+        console.log("shop name : ", shopName);
+        ShopActions.select_shop({shopName, index});
+
+        Actions.homeDetailScreen()
+    }
 
     render() {
+        const {shopNames} = this.props;
+
         return (
             <ScrollView style={styles.scrollViewContainer}>
                 {
-                    this.state.shopNames.map((item, index) => (
+                    shopNames.map((item, index) => (
                         <Card transparent key={item.id} style={styles.cardContainer}>
                             <TouchableWithoutFeedback2
-                                onPress={() => Actions.homeDetailScreen()}
+                                onPress={() => this.handleSelectShop(item.name, index)}
                             >
                                 <View>
                                     <CardItem cardBody>
                                         <Image
-                                            source={{uri: item.uri}}
+                                            source={item.uri}
                                             style={{height: 200, flex: 1, borderRadius: 10, margin: 15}}/>
                                     </CardItem>
                                     <CardItem style={styles.cardItem}>
@@ -114,4 +88,11 @@ const styles = StyleSheet.create({
     }
 });
 
-export default ShopList;
+export default connect(
+    (state) => ({
+        shopNames: state.shop.get('shopNames')
+    }),
+    (dispatch) => ({
+        ShopActions: bindActionCreators(shopActions, dispatch)
+    })
+)(ShopList);
